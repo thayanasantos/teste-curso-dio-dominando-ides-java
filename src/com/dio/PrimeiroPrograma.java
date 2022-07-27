1952fab39fbb03b00ee8e14934e6c0393c1a0912
thayanasantos <thayanasistemas@gmail.com>
2022-07-26 21:06:44 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,32 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+
+
+	public static void main(String[] args) {
+	
+		
+		
+		Livro livro = new Livro ();
+		 Gato gato = new Gato ();
+		 
+		
+     System.out.println(gato);
+	System.out.println(livro);
+     
+    
+	}
+	
+	
+  
+}
+class Livro{
+	
+	private String nome; 
+	private String npag; 
+}
+
+
